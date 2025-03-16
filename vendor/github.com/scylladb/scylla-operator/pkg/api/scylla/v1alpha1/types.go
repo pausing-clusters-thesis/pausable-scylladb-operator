3581cfc18a0bae200c4946c995ea41ed3a7eea2e
Kacper Rzetelski <rzetelski.k@gmail.com>
2025-03-16 17:33:40 +0100

    Update vendor

---
+++ vendor/github.com/scylladb/scylla-operator/pkg/api/scylla/v1alpha1/types.go
@@ -0,0 +1,10 @@
+// Copyright (C) 2024 ScyllaDB
+
+package v1alpha1
+
+const (
+	AvailableCondition   = "Available"
+	ProgressingCondition = "Progressing"
+	DegradedCondition    = "Degraded"
+	PrewarmedCondition   = "Prewarmed"
+)
